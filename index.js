24c056db24010bf87b3c638cf4920436f90f2898
Maxime <maxime@MacBook-Pro-2.local>
2024-06-17 11:45:48 +0200

    sticky navigation

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.onscroll = stickyNav();
+
+https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+*/
