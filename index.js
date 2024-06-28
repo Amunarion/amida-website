df6c7394e6f9a2d6078b0ba91221056ad2d39ed6
Maxime <maxime@MacBook-Pro-2.local>
2024-06-28 15:55:28 +0200

    added mobile navigation

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ window.onscroll = stickyNav();
 
 https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
 */
+
+function toggleMenu() {
+  const menu = document.getElementById("navbar-mobile-menu");
+  const navbarMobile = document.getElementById("navbar-mobile");
+
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    navbarMobile.style.backgroundColor = "transparent";
+  } else {
+    menu.style.display = "block";
+    navbarMobile.style.backgroundColor = "black";
+  }
+}
